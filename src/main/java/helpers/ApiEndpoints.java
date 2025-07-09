1939ef87cdeda769d4fb35999d79775ea457ea4c
Illia <qa.kond.work@gmail.com>
2025-07-09 12:33:15 +0200

    Add: ApiEndpoints.java

---
+++ src/main/java/helpers/ApiEndpoints.java
@@ -0,0 +1,14 @@
+package helpers;
+
+public class ApiEndpoints {
+
+    public static final String LOGIN = "/users/login";
+    public static final String LOGOUT = "/users/logout";
+    public static final String PROFILE = "/users/profile";
+    public static final String FORGOT_PASSWORD = "/users/forgot_password";
+    public static final String RESET_PASSWORD = "/users/reset_password";
+    public static final String CHANGE_PASSWORD = "/users/change_password";
+    public static final String RESET_PASSWORD_TOKEN = "/users/verify-reset-password-token";
+    public static final String NOTES = "/notes";
+
+}
