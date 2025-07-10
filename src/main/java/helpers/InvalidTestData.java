d994ddf26cb719bcb6715dd86d9c42584fd3eb2a
Illia <qa.kond.work@gmail.com>
2025-07-10 17:57:40 +0200

    Add: InvalidTestData.java

---
+++ src/main/java/helpers/InvalidTestData.java
@@ -0,0 +1,6 @@
+package helpers;
+
+public class InvalidTestData {
+    public static final String WRONG_PASSWORD = "wrongPass123";
+    public static final String INVALID_EMAIL = "test_invalid@qa.test";
+}
