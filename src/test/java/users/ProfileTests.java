b3239012f32e03cbae7efec528a621e5d8afd2ba
Illia <qa.kond.work@gmail.com>
2025-07-11 10:46:40 +0200

    Add: ProfileTests.java(positive tests)

---
+++ src/test/java/users/ProfileTests.java
@@ -0,0 +1,59 @@
+package users;
+
+import helpers.ApiEndpoints;
+import helpers.AuthHelper;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+
+import java.sql.SQLOutput;
+import java.util.ResourceBundle;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ProfileTests {
+
+    ResourceBundle credentials = ResourceBundle.getBundle("config");
+    ResourceBundle userMessages = ResourceBundle.getBundle("messages.user_messages");
+
+    String baseUrl = credentials.getString("base.url");
+
+    @Test
+    public void getProfileReturnsSuccess() {
+        String token = AuthHelper.generateToken();
+
+        String getSuccessMessage = userMessages.getString("profile.successful");
+
+       given()
+                .contentType(ContentType.JSON)
+                .header("x-auth-token", token)
+                .when()
+                .get(baseUrl + ApiEndpoints.PROFILE)
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("message", equalTo(getSuccessMessage));
+    }
+
+    @Test
+    public void patchProfileReturnsSuccess() {
+        String token = AuthHelper.generateToken();
+
+        String name = credentials.getString("user.name");
+        String phone = credentials.getString("user.phone");
+        String company = credentials.getString("user.company");
+
+        String patchSuccessMessage = userMessages.getString("profile.patch");
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("x-auth-token", token)
+                .body(String.format("{\"name\":\"%s\", \"phone\":\"%s\", \"company\":\"%s\"}", name, phone, company))
+                .when()
+                .patch(baseUrl + ApiEndpoints.PROFILE)
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("message", equalTo(patchSuccessMessage));
+    }
+}
