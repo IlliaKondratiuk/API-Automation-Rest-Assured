aa998b09af01e140b50d476e4c71f808abb64ce0
Illia <qa.kond.work@gmail.com>
2025-07-15 20:31:13 +0200

    Add: positive test in PostNoteTest.java

--- src/test/java/notes/PostNoteTest.java
+++ src/test/java/notes/PostNoteTest.java
@@ -1,32 +1,40 @@
 package notes;
 
+import helpers.ApiEndpoints;
 import helpers.AuthHelper;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
 import java.util.ResourceBundle;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostNoteTest {
 
-    ResourceBundle credentials = ResourceBundle.getBundle("config");
+    ResourceBundle common = ResourceBundle.getBundle("common.common_info");
+    ResourceBundle info = ResourceBundle.getBundle("note_info");
     ResourceBundle notesMessages = ResourceBundle.getBundle("messages.notes_messages");
-    ResourceBundle commonMessages = ResourceBundle.getBundle("messages.common_messages");
 
-    String baseUrl = credentials.getString("base.url");
+    String baseUrl = common.getString("base.url");
 
     @Test
     public void validPatchNoteReturns200() {
         String token = AuthHelper.generateToken();
 
-        String title =
+        String title = info.getString("title");
+        String description = info.getString("description");
+        String category = info.getString("valid_category");
 
         given()
                 .contentType(ContentType.JSON)
                 .header("x-auth-token", token)
-                .body(String.format("{\"name\":\"%s\", \"phone\":\"%s\", \"company\":\"%s\"}", title, description, category))
-
+                .body(String.format("{\"title\":\"%s\", \"description\":\"%s\", \"category\":\"%s\"}", title, description, category))
+        .when()
+                .post(baseUrl + ApiEndpoints.NOTES)
+        .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("message", equalTo(notesMessages.getString("note.patch.created")));
     }
 }
