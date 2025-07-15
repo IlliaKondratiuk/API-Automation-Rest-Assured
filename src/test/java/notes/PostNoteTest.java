b5e234948ee039f89ba52befc9be46288e05e196
Illia <qa.kond.work@gmail.com>
2025-07-15 19:53:01 +0200

    Add: note_info.properties

--- src/test/java/notes/PostNoteTest.java
+++ src/test/java/notes/PostNoteTest.java
@@ -1,6 +1,13 @@
 package notes;
 
+import helpers.AuthHelper;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
 import java.util.ResourceBundle;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 
 public class PostNoteTest {
 
@@ -10,5 +17,16 @@ public class PostNoteTest {
 
     String baseUrl = credentials.getString("base.url");
 
+    @Test
+    public void validPatchNoteReturns200() {
+        String token = AuthHelper.generateToken();
 
+        String title =
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("x-auth-token", token)
+                .body(String.format("{\"name\":\"%s\", \"phone\":\"%s\", \"company\":\"%s\"}", title, description, category))
+
+    }
 }
