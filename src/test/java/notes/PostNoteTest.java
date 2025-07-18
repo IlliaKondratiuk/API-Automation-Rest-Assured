a315198cdc5e800b79357df6b9abcee2ce79cee6
Illia <qa.kond.work@gmail.com>
2025-07-18 11:39:18 +0200

    Add: negative tests to PostNoteTest.java

--- src/test/java/notes/PostNoteTest.java
+++ src/test/java/notes/PostNoteTest.java
@@ -16,6 +16,7 @@ public class PostNoteTest {
     ResourceBundle common = ResourceBundle.getBundle("common.common_info");
     ResourceBundle info = ResourceBundle.getBundle("note_info");
     ResourceBundle notesMessages = ResourceBundle.getBundle("messages.notes_messages");
+    ResourceBundle commonMessages = ResourceBundle.getBundle("messages.common_messages");
 
     String baseUrl = common.getString("base.url");
 
@@ -36,5 +37,42 @@ public class PostNoteTest {
         .then()
                 .statusCode(HttpStatus.SC_OK)
                 .body("message", equalTo(notesMessages.getString("note.patch.created")));
+    }
+
+    @Test
+    public void invalidPostNoteReturns400() { //missing category
+        String token = AuthHelper.generateToken();
+
+        String title = info.getString("title");
+        String description = info.getString("description");
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("x-auth-token", token)
+                .body(String.format("{\"title\":\"%s\", \"description\":\"%s\"}", title, description))
+                .when()
+                .post(baseUrl + ApiEndpoints.NOTES)
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("message", equalTo(notesMessages.getString("note.patch.badrequest")));
+    }
+
+    @Test
+    public void unauthorizedPostNoteReturns401() { //missing category
+        String token = AuthHelper.generateToken() + "1";
+
+        String title = info.getString("title");
+        String description = info.getString("description");
+        String category = info.getString("valid_category");
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("x-auth-token", token)
+                .body(String.format("{\"title\":\"%s\", \"description\":\"%s\", \"category\":\"%s\"}", title, description, category))
+                .when()
+                .post(baseUrl + ApiEndpoints.NOTES)
+                .then()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
+                .body("message", equalTo(commonMessages.getString("unauthorized")));
     }
 }
