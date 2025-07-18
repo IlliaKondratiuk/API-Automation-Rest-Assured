9248736593774f65ac79e8154e665f33c0e13d12
Illia <qa.kond.work@gmail.com>
2025-07-18 12:21:07 +0200

    Fix: getters in GetNotesResponse.java

--- src/main/java/models/GetNotesResponse.java
+++ src/main/java/models/GetNotesResponse.java
@@ -7,6 +7,21 @@ public class GetNotesResponse {
     private boolean success;
     private int status;
     private String message;
-    private List<List<Note>> data;
+    private List<Note> data;
 
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<Note> getData() {
+        return data;
+    }
 }
