259793f190f47fea37e443e0657d5c4394d7bed9
Illia <qa.kond.work@gmail.com>
2025-07-28 13:37:45 +0200

    Add: negative tests to PutNoteTest.java

--- src/test/java/notes/PutNoteTest.java
+++ src/test/java/notes/PutNoteTest.java
@@ -20,7 +20,6 @@ public class PutNoteTest {
 
     String baseUrl = common.getString("base.url");
 
-
     @Test
     public void putNoteReturns200() {
         String token = AuthHelper.generateToken();
@@ -44,5 +43,48 @@ public class PutNoteTest {
                 .statusCode(HttpStatus.SC_OK)
                 .body("message", equalTo(expectedMessage));
     }
+
+    @Test
+    public void invalidPutNoteReturns400() { // missing id
+        String token = AuthHelper.generateToken();
+
+        String id = noteInfo.getString("note.id");
+        String expectedMessage = commonMessages.getString("badrequest.emptybody");
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("x-auth-token", token)
+                .body(" ")
+        .when()
+                .patch(baseUrl + ApiEndpoints.NOTES + "/" + id)
+        .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("message", equalTo(expectedMessage));
+    }
+
+    @Test
+    public void unauthorizedPutNoteReturns401() { // incorrect token
+        String token = AuthHelper.generateToken() + "1"; // deliberately invalid
+
+        String id = noteInfo.getString("note.id");
+        String title = noteInfo.getString("note.title");
+        String description = noteInfo.getString("note.description");
+        String completed = noteInfo.getString("note.completed.true");
+        String category = noteInfo.getString("note.category.work");
+
+        String expectedMessage = commonMessages.getString("unauthorized");
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("x-auth-token", token)
+                .body(String.format("{\"id\":\"%s\", \"title\":\"%s\", \"description\":\"%s\", \"completed\":\"%s\", \"category\":\"%s\"}",
+                        id, title, description, completed, category))
+                .when()
+                .patch(baseUrl + ApiEndpoints.NOTES + "/" + id)
+                .then()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
+                .body("message", equalTo(expectedMessage));
+    }
+
 
 }
